90bdd2dd80b89bd3d9b3f9e440061fb00e887aaa
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-01-27 23:29:48 +0530

    Digit Frequency

--- Printing-Tokens/main.c
+++ Printing-Tokens/main.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 int main()
 {
-    char str1[100];
-    char newString[10][10];
-    int i,j,ctr;
-
-
-
-    fgets(str1, sizeof str1, stdin);
-
-    j=0; ctr=0;
-    for(i=0;i<=(strlen(str1));i++)
-    {
-        // if space or NULL found, assign NULL into newString[ctr]
-        if(str1[i]==' '||str1[i]=='\0')
-        {
-            newString[ctr][j]='\0';
-            ctr++;  //for next word
-            j=0;    //for next word, init index to 0
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    int len = strlen(s);
+    for(int i = 0; i < len; i++) {
+        if(s[i] == ' ') {
+            printf("\n");
         }
-        else
-        {
-            newString[ctr][j]=str1[i];
-            j++;
+        else {
+            printf("%c", s[i]);
         }
     }
-
-    for(i=0;i < ctr;i++)
-        printf(" %s\n",newString[i]);
+    free(s);
     return 0;
 }
